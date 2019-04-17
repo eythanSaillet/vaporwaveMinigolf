8a58af77463d548c15fd5586dfc59b225d354036
Aurelien Sarrat <aurelien.sarrat@gmail.com>
2019-04-17 18:49:21 +0200

    Uptade for Song Problems

--- vaporgolf/scripts/gamePage.js
+++ vaporgolf/scripts/gamePage.js
@@ -1,8 +1,9 @@
 let music = new Audio ('sounds/music.mp3');
 
-music.play();
-music.volume= 0.15;
-
+setTimeout(() => {
+  music.play();
+  music.volume=0.15;
+},500)
 
 let playerSelector = document.querySelectorAll(".section_numberPlayer_player")
 let numberPlayerMenu = document.querySelector(".section_numberPlayer")
